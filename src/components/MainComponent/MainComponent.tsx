38a48f6b445e7958d990e2ddde824916800e7638
Luana Oliveiram <luanasilva.jua@gmail.com>
2023-08-15 17:42:55 -0300

    Implementa CSS da Main

--- src/components/MainComponent/MainComponent.tsx
+++ src/components/MainComponent/MainComponent.tsx
@@ -5,13 +5,35 @@ import style from './MainComponent.module.css'
 export default function Main() {
     return(
         <div className={style.main}>
-            <h1 className={style.title}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. In, earum?</h1>
-            {/* <Image className={style.background} src={background} alt="Image do fundo"/> */}
+            <div className={style.title}>
+                <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. In, earum?</h1>
+            </div>
             <div>
                 <ul className={style.group}>
-                    <li className={style.element}></li>
-                    <li className={style.element}></li>
-                    <li className={style.element}></li>
+                    <li>
+                        <p className={style.group_title}>Lorem</p>
+                        <p className={style.group_text}>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore dolore molestiae sequi animi
+                            possimus blanditiis officiis quibusdam sunt deleniti magnam aut omnis impedit, aliquam alias 
+                            minus tenetur sed. Commodi, odit!
+                        </p>
+                    </li>
+                    <li className={style.group}>
+                        <p className={style.group_title}>Lorem.</p>
+                        <p className={style.group_text}>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A fugit aspernatur vitae hic impedit
+                            libero maxime rem eos in tempora? Voluptates minus fugit deserunt praesentium repudiandae. 
+                            Ab temporibus repudiandae molestiae.
+                        </p>
+                    </li>
+                    <li className={style.group}>
+                        <p className={style.group_title}>Lorem</p>
+                        <p className={style.group_text}>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque blanditiis ex et similique 
+                            pariatur porro incidunt eum aliquam? Ipsum voluptate eligendi inventore commodi, quaerat natus 
+                            voluptatem ratione pariatur ullam nesciunt.
+                        </p>
+                    </li>
                 </ul>
             </div>
 
